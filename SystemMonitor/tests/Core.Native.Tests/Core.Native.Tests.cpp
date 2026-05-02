dcd6420f888aab76ef5b94c733825b7900f53153
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-05-02 23:13:34 +0300

    Implement Testing for CPU

--- SystemMonitor/tests/Core.Native.Tests/Core.Native.Tests.cpp
+++ SystemMonitor/tests/Core.Native.Tests/Core.Native.Tests.cpp
@@ -1,20 +1,25 @@
-// Core.Native.Tests.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <Windows.h>
+#include "cpu_reader.h"
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    hardwares::cpu::clsCpu_Reader cpu_reader;
+
+    std::cout << "--- System Monitor: CPU Test Bench ---\n" << std::endl;
+
+    for (short i = 0; i < 10; i++)
+    {
+        double cpu_usage = cpu_reader.getCpuUsage();
+
+        cout << "CPU Usage: " << cpu_usage << " %" << endl;
+
+        Sleep(1000); // sleep for 1 second.
+    }
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
