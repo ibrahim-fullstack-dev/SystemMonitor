6352ce21b95f76b4b933abd9e1171ce4b1ca3fc5
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-05-03 09:00:02 +0300

    Testing Ram Implementation

--- SystemMonitor/tests/Core.Native.Tests/Core.Native.Tests.cpp
+++ SystemMonitor/tests/Core.Native.Tests/Core.Native.Tests.cpp
@@ -1,24 +1,37 @@
-
 #include <iostream>
 #include <Windows.h>
 #include "cpu_reader.h"
-#include "cpu_reader.h"
+#include "ram_reader.h"
 
 
 using namespace std;
 
 int main()
 {
-    hardwares::cpu::clsCpu_Reader cpu_reader;
+    hardwares::cpu::clsCpu_Reader cpu;
+    hardwares::ram::clsRam_reader ram;
 
 
-    std::cout << "--- System Monitor: CPU Test Bench ---\n" << std::endl;
+    cout << "========================================" << endl;
+    cout << "   System Monitor - Hardware Test       " << endl;
+    cout << "========================================" << endl;
 
     for (short i = 0; i < 10; i++)
     {
-        double cpu_usage = cpu_reader.getCpuUsage();
+        double cpuUsage = cpu.getCpuUsage();
+        int ramPercent = ram.getRamUsagePercentage();
+        double totalRam = ram.getTotalRamGB();
+        double usedRam = ram.getUsedRamGB();
+        double totalVirtual = ram.getVirtualRamGB();
+        double usedVirtual = ram.getUsedVirtualRamGB();
 
-        cout << "CPU Usage: " << cpu_usage << " %" << endl;
+        system("cls"); // clear console
+
+        cout << "[CPU]: " << cpuUsage << "%" << endl;
+        cout << "[RAM]: " << ramPercent << "% (" << usedRam << "GB / " << totalRam << "GB)" << endl;
+        cout << "[VRM]: " << usedVirtual << "GB / " << totalVirtual << "GB (Virtual)" << endl;
+        cout << "----------------------------------------" << endl;
+
 
         Sleep(1000); // sleep for 1 second.
     }
