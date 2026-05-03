15f4915b55050b52360fa0f5e3e29ba483b3b4bc
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-05-03 10:34:10 +0300

    test storage.cpp

--- SystemMonitor/tests/Core.Native.Tests/Core.Native.Tests.cpp
+++ SystemMonitor/tests/Core.Native.Tests/Core.Native.Tests.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include "cpu_reader.h"
 #include "ram_reader.h"
+#include "storage_reader.h"
 
 
 using namespace std;
@@ -10,6 +11,7 @@ int main()
 {
     hardwares::cpu::clsCpu_Reader cpu;
     hardwares::ram::clsRam_reader ram;
+    hardwares::storage::clsStorage_reader storage;
 
 
     cout << "========================================" << endl;
@@ -30,10 +32,25 @@ int main()
         cout << "[CPU]: " << cpuUsage << "%" << endl;
         cout << "[RAM]: " << ramPercent << "% (" << usedRam << "GB / " << totalRam << "GB)" << endl;
         cout << "[VRM]: " << usedVirtual << "GB / " << totalVirtual << "GB (Virtual)" << endl;
+
         cout << "----------------------------------------" << endl;
 
 
         Sleep(1000); // sleep for 1 second.
+    }
+
+    for (const wstring& drive : storage.getDriveList()) {
+
+        double totalSpace = storage.getTotalSpaceGB(drive);
+        double freeSpace = storage.getFreeSpaceGB(drive);
+        double usedSpace = storage.getUsedSpaceGB(drive);
+        int usagePercent = storage.getUsagePercentage(drive);
+
+        wcout << "Drive: " << drive.c_str() << endl;
+        cout << "  Total Space: " << totalSpace << " GB" << endl;
+        cout << "  Free Space: " << freeSpace << " GB" << endl;
+        cout << "  Used Space: " << usedSpace << " GB" << endl;
+		cout << "  Usage Percentage: " << usagePercent << "%" << endl;
     }
     return 0;
 }
