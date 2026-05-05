12173a0554f9ac7de67f37510122d6bdcee31b9b
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-05-05 18:29:59 +0300

    feat: structure MVVM pattern with CPU

--- SystemMonitor/src/UI.Desktop/viewModels/clsCPUViewModel.cs
+++ SystemMonitor/src/UI.Desktop/viewModels/clsCPUViewModel.cs
@@ -1,12 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UI.Desktop.Global;
+using UI.Desktop.Models;
+using UI.Desktop.Services.HardwareSevices;
 
-namespace UI.Desktop.ViewModels
+namespace UI.Desktop.ViewModels;
+
+public class clsCPUViewModel: clsBaseClass, IDisposable
 {
-    internal class clsCPUViewModel
+    private clsCPUService _cpuService;
+    private clsCpuModel _cpuModel;
+    public clsCpuModel CPUUsagePercentage
     {
+        get => _cpuModel;
+        set
+        {
+            _cpuModel = value; OnPropertyChanged();
+            // NotifyPropertyChanged if implementing INotifyPropertyChanged
+        }
     }
+
+    private clsCpuModel GetFullReport()
+    {
+        return new clsCpuModel
+        {
+            UsagePercentage = _cpuService.GetCpuUsage(),
+        };
+    }
+
+    public clsCPUViewModel()
+    {
+        _cpuService = new clsCPUService();
+        UpdateCPUData();
+        // Initialize properties or start monitoring CPU data here
+    }
+
+    public void UpdateCPUData()
+    {
+        _cpuModel = GetFullReport();
+        // Implement logic to update CPU usage and temperature data here
+    }
+
+    public void Dispose()
+    {
+            _cpuService?.Dispose();
+        // Clean up any resources if necessary when the ViewModel is disposed
+    }
+
+
 }
