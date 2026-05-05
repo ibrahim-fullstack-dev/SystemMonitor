258de6faca62276590e12564bbc8de947919538e
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-05-05 10:11:56 +0300

    Create Services folder, create CPU cls and fetch CPU info

---
+++ SystemMonitor/src/UI.Desktop/services/hardwareSevices/clsCPU.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using share.core;
+
+namespace UI.Desktop.services
+{
+    namespace hardwareSevices {
+        public class clsCPU
+        {
+            public double GetCpuUsage()
+            {
+                try
+                {
+                    clsCpuWrapper CPU = new clsCpuWrapper();
+                    return CPU.getCPUUsage();
+
+                }
+                catch (Exception ex)
+                {
+                    return 0;
+                }
+
+            }
+        }
+    }
+
+    
+}
