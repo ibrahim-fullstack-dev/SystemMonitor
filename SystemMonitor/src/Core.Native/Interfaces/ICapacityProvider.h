871fb535b6d84c34e5607090b045da5b6a5d7522
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-05-09 21:19:39 +0300

    feat(capacity): create CapacityProvider interface

--- SystemMonitor/src/Core.Native/Interfaces/ICapacityProvider.h
+++ SystemMonitor/src/Core.Native/Interfaces/ICapacityProvider.h
@@ -1,1 +1,11 @@
-#pragma once
+#pragma once // Ensure the header is included only once during compilation
+
+namespace Interfaces {
+	class ICapacityProvider {
+	public:
+		virtual ~ICapacityProvider() = default; // Virtual destructor for proper cleanup of derived classes (cleanup resources from vTable).
+		virtual double GetCapacity() = 0;
+		virtual double GetAvailableCapacity() = 0;
+	};
+}
+
