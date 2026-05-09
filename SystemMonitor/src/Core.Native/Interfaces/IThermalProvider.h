71a634e8bc93963d8bb045283c37528a227cd5da
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-05-09 21:11:01 +0300

    feat(usage): create ThermalProvider interface

--- SystemMonitor/src/Core.Native/Interfaces/IThermalProvider.h
+++ SystemMonitor/src/Core.Native/Interfaces/IThermalProvider.h
@@ -1,1 +1,10 @@
-#pragma once
+#pragma once // Ensure the header is included only once during compilation
+
+namespace Interfaces {
+	class IThermalProvider {
+	public:
+		virtual ~IThermalProvider() = default; // Virtual destructor for proper cleanup of derived classes (cleanup resources from vTable).
+		virtual double GetTemperature() = 0;
+	};
+}
+
